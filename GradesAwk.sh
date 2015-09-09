0e20308211499a5ea909b120cf97487809daa4c0
Parth Mishra <parthmishra@Parths-MacBook-Pro.local>
2015-09-09 16:58:23 -0600

    GradesAwk.sh should work now, needs cleanup

--- GradesAwk.sh
+++ GradesAwk.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
+# Parth Mishra Chris Leung - HW 1
 
+# Usage Warning
+if [ $# -ne 1 ]
+then 
+	echo "Usage: GradesAwk.sh filename"
+	exit 1
+fi
+
+filename="$1"
+
+# awk command to calculate averages of each student and sort ouput
+awk '
+BEGIN {}
+{ $5 = ($4+$5+$6)/3 }
+{ print $5, "[" $1 "]", $3",",$2 }
+END {}
+' $filename | sort -k3 -k4 -k2
 
